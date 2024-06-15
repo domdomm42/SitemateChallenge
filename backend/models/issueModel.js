48433a5c784f140cf9a53a77119f25059bbd68a2
dom <limoudom2001@gmail.com>
2024-06-15 15:38:40 +1000

    feat: add custom UUID

--- backend/models/issueModel.js
+++ backend/models/issueModel.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 
 const issueSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuidv4,
+    unique: true,
+  },
   title: {
     type: String,
     required: true,
